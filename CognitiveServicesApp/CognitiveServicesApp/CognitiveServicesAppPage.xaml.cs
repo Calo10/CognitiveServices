c9f292647107f0e7ea5ad5a45e1fced13823f3b1
cmendez <carlos.mendez@avantica.net>
2017-09-28 22:43:37 -0600

    Create Solution

---
+++ CognitiveServicesApp/CognitiveServicesApp/CognitiveServicesAppPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CognitiveServicesApp
+{
+    public partial class CognitiveServicesAppPage : ContentPage
+    {
+        public CognitiveServicesAppPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
